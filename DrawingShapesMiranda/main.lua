fdbc25a83bcff098651d91b8f30d4fda168b59fd
Miranda Bennett <miranda.bennett@ocsbstudent.ca>
2020-03-03 15:26:58 -0500

    added to drawing shapes

--- DrawingShapesMiranda/main.lua
+++ DrawingShapesMiranda/main.lua
@@ -18,9 +18,14 @@ local myHexagon
 local triangleWidth = 40
 local triangleHeight = 70
 local areaOfTriangle
-local areatext
+local areatextAreaofTriangle
 local textSize = 40
-
+local areaTextPentagon
+local areaTextTrapazoid
+local areaTextTriangle
+local areaTextHexagon
+local textSize = 40
+local areaTextCredit
 --hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
 
@@ -47,6 +52,12 @@ myPentagon:setFillColor(157/255, 228/255, 233/255)
 --set color of the border
 myPentagon:setStrokeColor(153/255, 204/255, 255/255)
 
+--label my pentagram
+areaTextPentagon = display.newText("Pentagram", 150,200, Arial,textSize)
+
+--colour the text 
+areaTextPentagon:setTextColor (0/255, 0/255, 0/255)
+
 --display my trapazoid
 myTrapazoid = display.newPolygon(600, 10, verticesTrapazoid)
 
@@ -62,6 +73,12 @@ myTrapazoid.strokeWidth = 20
 --set colour of trapazoid
 myTrapazoid:setFillColor(228/255, 72/255, 129/255)
 myTrapazoid:setStrokeColor(204/255, 51/255, 153/255)
+
+--label my trapazoid
+areaTextTrapazoid = display.newText("Trapazoid", 740, 200, Arial, textSize)
+
+--set colour of text
+areaTextTrapazoid:setTextColor(0/255, 0/255, 0/255)
 
 -- display my triangle
 myTriangle = display.newPolygon(10, 700, verticesTriangle)
@@ -80,11 +97,17 @@ myTriangle.strokeWidth = 20
 myTriangle:setFillColor(80/255, 57/255, 228/255)
 myTriangle:setStrokeColor(102/255, 153/255, 255/255)
 
+--label my triangle
+areaTextTriangle = display.newText("Triangle", 200,650, Arial, textSize)
+
+--set colour of text
+areaTextTriangle:setTextColor(0/255, 0/255, 0/255)
+
 --calculate the area of the triangle 
 areaOfTriangle = triangleHeight * triangleWidth/2
 
 --display my hexagon 
-myHexagon = display.newPolygon(600, 400, verticesHexagon)
+myHexagon = display.newPolygon(580, 400, verticesHexagon)
 
 -- anchor my hexagon
 myHexagon.anchorX = 0
@@ -98,3 +121,23 @@ myHexagon.strokeWidth = 20
 --set colour of hexagon
 myHexagon:setFillColor(176/255, 245/255, 151/255)
 myHexagon:setStrokeColor(58/255, 238/255, 58/255)
+
+--label my Hexagon
+areaTextHexagon = display.newText("Hexagon", 770, 580,Arial, textSize )
+
+-- set colour of text
+areaTextHexagon:setTextColor(0/255, 0/255, 0/255)
+
+--give credit
+areaTextCredit = display.newText("By:Miranda", 512, 740,Arial, textSize)
+
+--set colour of credit
+areaTextCredit:setTextColor(0/255, 0/255,0/255)
+
+-- display area of triangle 
+areatextAreaofTriangle = display.newText("The area of this triangle with a width of \n "
+ .. triangleWidth .. " and the height of " .. triangleHeight .. " is " ..
+  areaOfTriangle .. " pixels²." , 270, 550, Arial, 30)
+
+-- set colour of text
+areatextAreaofTriangle:setTextColor(0/255,0/255,0/255)
