eff7fa2500ff82b1292613f4341acb39af8ff8da
Miranda Bennett <miranda.bennett@ocsbstudent.ca>
2020-03-06 20:35:17 -0500

    created and added to teach and react

---
+++ TouchAndReactMiranda.b/main.lua
@@ -0,0 +1,53 @@
+-----------------------------------------------------------------------------------------
+-- Title: TouchAndReact
+-- Name: Miranda
+-- Course: ICS2O
+-- This program...
+-----------------------------------------------------------------------------------------
+
+-- set background colour
+display.setDefault("background", 151/255, 229/255, 216/255)
+
+--hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+--create the blue button, set its position and make it visible
+local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png", 198, 96)
+blueButton.x = display.contentWidth/2
+blueButton.y = display.contentHeight/2
+blueButton.isVisible = true 
+
+-- create the red button, set its position and make it invisible
+local redButton = display.newImageRect("Images/Fast Button Active@2x.png", 198,96)
+redButton.x = display.contentWidth/2
+redButton.y = display.contentHeight/2
+redButton.isVisible = false
+
+--create text object, set its position and make it invisible
+local textObject = display.newText("Clicked!" , 0, 0, nil, 50)
+textObject.x = display.contentWidth/2
+textObject.y = display.contentHeight/3
+textObject:setTextColor(229/255, 151/255, 221/255)
+textObject.isVisible = false
+
+--Function:BlueButtonListener
+--Input:touch listener
+--Output: none
+--Description: when blue button is clicked, it will make the text appear with the red button,
+--and make the blue button disappear
+local function BlueButtonListener(touch)
+	if (touch.phase == "began") then
+		blueButton.isVisible = false
+		redButton.isVisible = true
+		textObject.isVisible = true
+	end 
+
+	if (touch.phase == "ended") then
+		blueButton.isVisible = true
+		redButton.isVisible = false
+		textObject.isVisible = false
+	end
+end
+ 
+--add the respective listenerto each object
+blueButton:addEventListener("touch", BlueButtonListener)
