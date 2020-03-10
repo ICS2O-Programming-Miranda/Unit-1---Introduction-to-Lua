744f897bbb8d66e125796379ec413a75b8938361
Miranda Bennett <miranda.bennett@ocsbstudent.ca>
2020-03-10 19:00:01 -0400

    added to num text field

--- NumTextFieldMiranda/main.lua
+++ NumTextFieldMiranda/main.lua
@@ -25,6 +25,7 @@ local randomNumber1
 local randomNumber2
 local userAnswer
 local correctAnswer
+local incorrectanswer
 
 ------------------------------------------------------------------------------------------
 --LOCAL FUNCATIONS
@@ -62,6 +63,11 @@ local function NumericFieldlistener( event )
 		if (userAnswer == correctAnswer) then 
 			correctObject.isVisible = true
 			timer.performWithDelay(3000 ,HideCorrect)
+		elseif (userAnswer == incorrectanswer) then
+			incorrectObject.isVisible = true
+			timer.performWithDelay(3000, HideCorrect)
+
+
 		end
 	end
 
@@ -72,3 +78,27 @@ end
  
  --displays a question and set the colour
  questionObject = display.newText ( "" , display.contentWidth/3, display.contentHeight/2, nil, 50)
+ questionObject:setTextColor(224/255, 255/255, 255/255)
+
+ --create the correct text object and make it invisible
+correctObject = display.newText("Correct!", display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+correctObject:setTextColor(224/255, 255/255, 255/255)
+correctObject.isVisible = false
+
+--create the incorrect text object and make it invisible
+incorrectObject = display.newText("Incorrect" , display.contentWidth/2, display.contentHeight*2/3, nil, 50)
+incorrectObject:setTextColor(255/255, 0/255, 0/255)
+incorrectObject.isVisible = false
+
+--create numeric field
+numericField = native.newTextField (display.contentWidth/2, display.contentHeight/2 , 150, 80)
+numericField.inputType = "number"
+
+--add the event listener for th numeric field
+numericField:addEventListener( "userInput" , NumericFieldlistener)
+
+------------------------------------------------------------------------------------------
+--FUNCTION CALLS
+-----------------------------------------------------------------------------------------
+-- call the function to ask the question
+AskQuestion()
